cd7d0f8ba548dd69dc185dea2dc14343a814b83d
shindjun <shindjun@gmail.com>
2025-06-10 02:05:12 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>안녕하세요! 고로 계산기입니다 🔥</h1>
+      <p>이 화면은 React 앱의 시작화면이에요.</p>
+    </div>
+  );
+}
+
+export default App;
